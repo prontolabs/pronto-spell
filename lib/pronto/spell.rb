6397357dc0c11362443307a9f11402e7321137c5
Povilas Jurčys <povilasjurcys@users.noreply.github.com>
2018-12-07 22:07:04 +0200

    Make spell checker smarter (#3)
    * Make the gem usable
    
    * Fix word length check
    
    * Skip words containing digits
    
    * Allow to configure spell checker and lint camel case words and ignore numbers
    
    * Add documentation for yml config files
    
    * Allow to run aspell from travis

--- lib/pronto/spell.rb
+++ lib/pronto/spell.rb
@@ -3,20 +3,33 @@ require 'ffi/aspell'
 
 module Pronto
   class Spell < Runner
-    def run
-      return [] unless @patches
+    CONFIG_FILE = '.pronto_spell.yml'.freeze
 
-      @patches.select { |patch| patch.additions > 0 }
+    def ignored_words
+      @ignored_words ||= begin
+        words = (spelling_config['ignored_words'] || []).map(&:downcase)
+        Set.new(words)
+      end
+    end
+
+    def run
+      return [] if !@patches || @patches.count.zero?
+
+      @patches
+        .select { |patch| patch.additions > 0 }
         .map { |patch| inspect(patch) }
         .flatten.compact
     end
 
+    private
+
     def inspect(patch)
       patch.added_lines.map do |line|
-        words = line.content.scan(/[0-9a-zA-Z]+/)
-        words.select { |word| word.length > 4 }
-          .uniq
-          .select { |word| !speller.correct?(word) }
+        words = line.content.scan(/([A-Z]{2,})|([A-Z]{0,1}[a-z]+)/)
+          .flatten.compact.uniq
+
+        words
+          .select { |word| misspelled?(word) }
           .map { |word| new_message(word, line) }
       end
     end
@@ -26,17 +39,51 @@ module Pronto
       level = :warning
 
       suggestions = speller.suggestions(word)
-      msg = "#{word} might not be spelled correctly. Spelling suggestions: #{suggestions}"
+
+      msg = %("#{word}" might not be spelled correctly.)
+      unless suggestions.empty?
+        msg += " Spelling suggestions: #{suggestions[0..2].join(', ')}"
+      end
 
       Message.new(path, line, level, msg, nil, self.class)
     end
 
     def speller
-      @speller ||= begin
-        result = FFI::Aspell::Speller.new('en_US')
-        result.suggestion_mode = 'fast'
-        result
+      @speller ||= FFI::Aspell::Speller.new(
+        'en_US', :'sug-mode' => suggestion_mode
+      )
+    end
+
+    def spelling_config
+      @spelling_config ||= begin
+        config_path = File.join(repo_path, CONFIG_FILE)
+        File.exist?(config_path) ? YAML.load_file(config_path) : {}
       end
+    end
+
+    def suggestion_mode
+      spelling_config['suggestion_mode'] || 'fast'
+    end
+
+    def min_word_length
+      spelling_config['min_word_length'] || 5
+    end
+
+    def max_word_length
+      spelling_config['max_word_length'] || Float::INFINITY
+    end
+
+    def max_suggestions_number
+      spelling_config['max_suggestions_number'] || 3
+    end
+
+    def misspelled?(word)
+      lintable_word?(word) && !speller.correct?(word)
+    end
+
+    def lintable_word?(word)
+      (min_word_length..max_word_length).cover?(word.length) &&
+        !ignored_words.include?(word.downcase)
     end
   end
 end
---
+++ spec/pronto/spell_spec.rb
@@ -0,0 +1,114 @@
+require 'spec_helper'
+
+module Pronto
+  describe Spell do
+    subject(:spell) { described_class.new(patches) }
+
+    let(:patches) { [patch] }
+
+    let(:patch) do
+      double('Patch', additions: 1, added_lines: added_lines)
+    end
+
+    let(:added_lines) do
+      [
+        double(
+          'AddedLine',
+          content: patch_content, patch: line_patch, commit_sha: 'abc23'
+        )
+      ]
+    end
+
+    let(:patch_content) { 'helllo woorld!' }
+    let(:line_patch) { double('LinePatch', delta: line_patch_delta) }
+    let(:line_patch_delta) { double('Delta', new_file: { file_path: 'some/path.txt' }) }
+    let(:spelling_config) { {} }
+
+    before do
+      allow(spell).to receive(:spelling_config).and_return(spelling_config)
+      allow(spell).to receive(:repo_path).and_return('/home/developer/projects/pronto-spell')
+    end
+
+    describe '#run' do
+      subject(:lint_messages) { spell.run.map(&:msg) }
+
+      context 'patches are nil' do
+        let(:patches) { nil }
+
+        it { is_expected.to be_empty }
+      end
+
+      context 'no patches' do
+        let(:patches) { [] }
+
+        it { is_expected.to be_empty }
+      end
+
+      context 'with misspelled words' do
+        it 'returns list of misspeled words' do
+          expect(lint_messages).to eq [
+            '"helllo" might not be spelled correctly. ' \
+              'Spelling suggestions: hell lo, hell-lo, hello',
+
+            '"woorld" might not be spelled correctly. ' \
+              'Spelling suggestions: world, wold, whorled'
+          ]
+        end
+      end
+
+      context 'with camel cased words' do
+        let(:patch_content) { 'CamelWithhWrrrongGrammar' }
+
+        it 'validates each word separately' do
+          expect(lint_messages).to eq [
+            '"Withh" might not be spelled correctly. ' \
+              'Spelling suggestions: With, Withe, Wither',
+
+            '"Wrrrong" might not be spelled correctly. ' \
+              'Spelling suggestions: Wrong, Wrung, Wring'
+          ]
+        end
+      end
+
+      context 'with words consisting of capital letters only' do
+        let(:patch_content) { 'LOOOK_AT_ME_I_AM_SHOWTING' }
+
+        it 'validates each word separately' do
+          expect(lint_messages).to eq [
+            '"LOOOK" might not be spelled correctly. ' \
+              'Spelling suggestions: LOO OK, LOO-OK, LOOK',
+
+            '"SHOWTING" might not be spelled correctly. ' \
+              'Spelling suggestions: SHOW TING, SHOW-TING, SHOOTING'
+          ]
+        end
+      end
+
+      context 'with words which contain numbers' do
+        let(:patch_content) { '2coool4skool' }
+
+        it 'lints words and ignores number part' do
+          expect(lint_messages).to eq [
+            '"coool" might not be spelled correctly. '\
+              'Spelling suggestions: cool, Colo, coil',
+
+            '"skool" might not be spelled correctly. '\
+              'Spelling suggestions: skoal, school, skill'
+          ]
+        end
+      end
+
+      context 'with ignored words' do
+        let(:spelling_config) do
+          { 'ignored_words' => ['HaXoR'] }
+        end
+
+        let(:patch_content) { 'it feels good to be a haxor' }
+
+        it 'does not complain about words included in personal dictionary' do
+          expect(lint_messages).to be_empty
+        end
+      end
+    end
+  end
+end
